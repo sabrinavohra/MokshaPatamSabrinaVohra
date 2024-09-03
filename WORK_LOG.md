cd9bcadb2a7f6a869aafaca91f085e213b176b35
sabrinavohra <sabrina.vohra@menloschool.org>
2024-09-02 22:29:22 -0700

    Wrote pseudocode

--- WORK_LOG.md
+++ WORK_LOG.md
@@ -1,9 +1,10 @@
 Name: Sabrina Vohra
 
-| Date |       Time       |                                                                             Update |
-|:-----|:----------------:|-----------------------------------------------------------------------------------:|
-| 8/29 |  2:55 - 3:00pm   |                                              Created project and shared on Github. |
-| 9/02 | 9:40pm - 10:00pm | Worked through creating an array to represent the board. Began writing pseudocode. |
+| Date |       Time       |                                                                                      Update |
+|:-----|:----------------:|--------------------------------------------------------------------------------------------:|
+| 8/29 |  2:55 - 3:00pm   |                                                       Created project and shared on Github. |
+| 9/02 | 9:40pm - 10:00pm |          Worked through creating an array to represent the board. Began writing pseudocode. |
+| 9/02 | 10:00 - 10:30pm  | Looked back on MazeSolver to brainstorm ideas and wrote pseudocode detailing possibilities. |
 
 
 To add a new row to the table, click into a cell and then hit shift-enter.
--- src/MokshaPatam.java
+++ src/MokshaPatam.java
@@ -13,6 +13,8 @@ public class MokshaPatam {
      * TODO: Complete this function, fewestMoves(), to return the minimum number of moves
      *  to reach the final square on a board with the given size, ladders, and snakes.
      */
+    // How do we know how long / wide the board is if we only have the size?
+    // What does "the first of the two integers leads to the next" mean?
     public static int fewestMoves(int boardsize, int[][] ladders, int[][] snakes) {
         int board[][] = new int[boardsize/10][10];
         for(int i = 0; i < board.length; i++) {
@@ -20,6 +22,19 @@ public class MokshaPatam {
                 // Iterate through board and add ladders and snakes where applicable
             }
         }
-        return 0;
+        // Iterate through board and take as many ladders as possible without reaching snakes
+        int i = 0;
+        int j = 0;
+        while(board[i][j] <= board[boardsize/10][10]) {
+            i++;
+            j++;
+        }
+        return -1;
     }
 }
+
+// Ideas
+    // Start from the end like Maze Solver and find the quickest route back to the beginning
+        // Keep track of how much each roll is depending on where the snakes and ladders are - don't necessarily roll 6 every time
+    // Work from the beginning and move through trying every outcome using recursion and printing the lowest one
+    // Document which ladder is the longest to make sure to take it
