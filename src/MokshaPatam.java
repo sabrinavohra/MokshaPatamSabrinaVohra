8b15f0c51cb2b6e33be57c67d2e52d5101454a0e
sabrinavohra <sabrina.vohra@menloschool.org>
2024-09-06 14:00:23 -0700

    Wrote return statement and debugged.

--- src/MokshaPatam.java
+++ src/MokshaPatam.java
@@ -25,63 +25,36 @@ public class MokshaPatam {
         int[] numRolls = new int[boardsize + 1];
         int rolls = 0;
         int[] laddersMap = new int[boardsize + 1];
-        for(int i = 0; i < ladders.length; i++) {
+        for (int i = 0; i < ladders.length; i++) {
             int start = ladders[i][0];
             int end = ladders[i][1];
             laddersMap[start] = end;
         }
         int[] snakesMap = new int[boardsize + 1];
-        for(int i = 0; i < snakes.length; i++) {
+        for (int i = 0; i < snakes.length; i++) {
             int start = snakes[i][0];
-            int end = snakes[i][i];
+            int end = snakes[i][1];
             snakesMap[start] = end;
         }
-        while(!toExplore.isEmpty()) {
+        while (!toExplore.isEmpty()) {
             currentNode = toExplore.remove();
-            if(currentNode == boardsize) {
+            if (currentNode == boardsize) {
                 return numRolls[currentNode];
             }
-            for(int r = 1; r <= 6; r++) {
+            for (int r = 1; r <= 6; r++) {
                 int node = currentNode + r;
-                if(laddersMap[node] != 0) {
+                if (laddersMap[node] > 0) {
                     node = laddersMap[node];
-                }
-                else if (snakesMap[node] != 0) {
+                } else if (snakesMap[node] > 0) {
                     node = snakesMap[node];
                 }
-                if(numRolls[node] == 0) {
+                if (numRolls[node] == 0) {
                     numRolls[node] = rolls;
                     toExplore.add(node);
                 }
                 rolls++;
             }
         }
-
-        // Creates board
-        int board[][] = new int[boardsize/10][10];
-        for(int i = 0; i < board.length; i++) {
-            for(int j = 0; j < board[0].length; j++ ) {
-                // Iterate through board and add ladders and snakes where applicable
-
-            }
-        }
-        // Iterates through board
-        int i = 0;
-        int j = 0;
-        while(board[i][j] <= board[boardsize/10][10]) {
-            i++;
-            j++;
-        }
-        // If no solution:
-        return -1;
-        // Make limit for if the roll rolls past the final square (ex. cannot roll 6 on square 95)
+        return 0;
     }
 }
-
-// Ideas:
-    // Start from the end like Maze Solver and find the quickest route back to the beginning
-        // Decide whether to take each square and follow each route cancelling out the higher numbers until a conclusion
-// is reached
-
-    // Work from the beginning and move through trying every outcome using recursion and printing the lowest one
-    // Document which ladder is the longest and make sure to take it
