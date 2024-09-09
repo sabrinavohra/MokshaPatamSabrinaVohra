6460d5581a44e226407842097e9af3883c528b0b
sabrinavohra <sabrina.vohra@menloschool.org>
2024-09-08 22:20:09 -0700

    Worked on debugging number of rolls

--- src/MokshaPatam.java
+++ src/MokshaPatam.java
@@ -16,29 +16,27 @@ public class MokshaPatam {
      * TODO: Complete this function, fewestMoves(), to return the minimum number of moves
      *  to reach the final square on a board with the given size, ladders, and snakes.
      */
-    // How do we know how long / wide the board is if we only have the size?
-    // What does "the first of the two integers leads to the next" mean?
     public static int fewestMoves(int boardsize, int[][] ladders, int[][] snakes) {
         Queue<Integer> toExplore = new LinkedList<>();
         toExplore.add(1);
         int currentNode;
-        int[] numRolls = new int[boardsize + 1];
         int rolls = 0;
+        int[] numRolls = new int[boardsize + 1];
         int[] laddersMap = new int[boardsize + 1];
-        for (int i = 0; i < ladders.length; i++) {
-            int start = ladders[i][0];
-            int end = ladders[i][1];
+        for (int[] ladder : ladders) {
+            int start = ladder[0];
+            int end = ladder[1];
             laddersMap[start] = end;
         }
         int[] snakesMap = new int[boardsize + 1];
-        for (int i = 0; i < snakes.length; i++) {
-            int start = snakes[i][0];
-            int end = snakes[i][1];
+        for (int[] snake : snakes) {
+            int start = snake[0];
+            int end = snake[1];
             snakesMap[start] = end;
         }
         while (!toExplore.isEmpty()) {
             currentNode = toExplore.remove();
-            if (currentNode == boardsize) {
+            if (currentNode >= boardsize) {
                 return numRolls[currentNode];
             }
             for (int r = 1; r <= 6; r++) {
@@ -54,9 +52,9 @@ public class MokshaPatam {
                         numRolls[node] = rolls;
                         toExplore.add(node);
                     }
-                    if(rolls > (node / 6)) {
-                        rolls--;
-                    }
+//                    if (rolls > (node / 6)) {
+//                        rolls--;
+//                    }
                 }
             }
             rolls++;
